6bb93c86bf5f60cdb73c70bcca77222a9c8b4a39
TriantafyllosFamprikatzis <alkisfab@gmail.com>
2019-01-31 22:28:43 +0200

    send button creates an alert message, an error is displayed when user is not selected or message is empty, Chart buttons now change color when clicked, message and user field clear when the buton is pressed

--- script.js
+++ script.js
@@ -1,30 +1,75 @@
 
 //VARIABLES
-const myChart = document.getElementById('myChart').getContext('2d');
-const myBarChart = document.getElementById('myBarChart').getContext('2d');
-const myDoughnutChart = document.getElementById('myDoughnutChart').getContext('2d');
-const hour = document.getElementById('h');
-const day = document.getElementById('d');
-const week = document.getElementById('w');
-const month = document.getElementById('m');
+const lineChart = document.getElementById('lineChart').getContext('2d');
+const barChart = document.getElementById('barChart').getContext('2d');
+const doughnutChart = document.getElementById('doughnutChart').getContext('2d');
+const hourChart = document.getElementById('hour');
+const dayChart = document.getElementById('day');
+const weekChart = document.getElementById('week');
+const monthChart = document.getElementById('month');
 
+const sendButton = document.getElementById('send');
 const deleteButton = document.getElementById('btn');
-const alert = document.getElementById('alert');
+const alertBar = document.getElementById('alert');
 
-// Alert Bar 
+const message = document.getElementById('textarea');
+const user = document.getElementById('search-bottom');
+
+// Alert Bar, hides when clicked 
 deleteButton.addEventListener('click', () => {
- alert.style.display = 'none';
+ alertBar.style.display = 'none';
+});
+
+//Bell notification
+
+
+
+// Alert on Send button
+/*
+When user press to submit the form, it displays a confirmation that the message was sent. 
+Error messages are displayed if a user isn’t selected or message field is empty.
+*/
+sendButton.addEventListener('click', () => {    
+ if (user.value == '') {
+    alert('Please select a User');
+ } else if (message.value == '') {
+   alert('Message is empty');
+ } else {
+    alert('Message successfully sent!');
+    user.value = '';
+    message.value = '';
+ }
 });
 
 
-//Bell notification
+
+//Chart Buttons
+/*Get all the relevant buttons into an array
+  Loop through the buttons
+  When the page loads add class to weekChart 
+  Set up a click event handler for the button
+  Loop though all the buttons and reset the colors back to default
+  Add the class to the one button that got clicked
+*/
+let buttons = Array.prototype.slice.call(document.querySelectorAll(".li"));
+buttons.forEach(function(btn){
+  weekChart.classList.add('liSelected');
+  btn.addEventListener("click", function(){
+    buttons.forEach(function(btn){ btn.classList.remove("liSelected"); });
+    this.classList.add("liSelected");
+  });
+});
+
+
+
 
 //Global chart defaults
 Chart.defaults.global.responsive = true;
 Chart.defaults.scale.ticks.beginAtZero = false;
 Chart.defaults.global.legend.display = false;
-//LINE CHART
-let chart = new Chart(myChart, {
+
+//LINE CHARTS
+let NewLineChart = new Chart(lineChart, {
     type: 'line',
     data: {
         labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
@@ -39,12 +84,10 @@ let chart = new Chart(myChart, {
         }
     ]
     },
-    options: {
-    }
 });
 
-hour.addEventListener('click', () => {
-    new Chart(myChart, {
+hourChart.addEventListener('click', () => {
+    new Chart(lineChart, {
         type: 'line',
         data: {
             labels: ["9am", "10am", "11pm", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"],
@@ -59,13 +102,11 @@ hour.addEventListener('click', () => {
             }
         ]
         },
-        options: {
-        }
     });
 });
 
-day.addEventListener('click', () => {
-    new Chart(myChart, {
+dayChart.addEventListener('click', () => {
+    new Chart(lineChart, {
         type: 'line',
         data: {
             labels: ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"],
@@ -85,8 +126,8 @@ day.addEventListener('click', () => {
     });  
 });
 
-week.addEventListener('click', () => {
-  new Chart(myChart, {
+weekChart.addEventListener('click', () => {
+  new Chart(lineChart, {
         type: 'line',
         data: {
             labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
@@ -101,13 +142,11 @@ week.addEventListener('click', () => {
             }
         ]
         },
-        options: {
-        }
     });
 });
 
-month.addEventListener('click', () => {
-    new Chart(myChart, {
+monthChart.addEventListener('click', () => {
+    new Chart(lineChart, {
         type: 'line',
         data: {
             labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
@@ -122,14 +161,12 @@ month.addEventListener('click', () => {
             }
         ]
         },
-        options: {
-        }
     });
 });
 
 
 //BAR CHART
-var barChart = new Chart(myBarChart, {
+let NewBarChart = new Chart(barChart, {
     type: 'bar',
     data: {
         labels: ["S", "M", "T", "W", "T", "F", "S"],
@@ -141,14 +178,12 @@ var barChart = new Chart(myBarChart, {
         }
     ]
     },
-    options: {
-    }
 });
 
 
 
 //DONUT CHART
-var doughnutChart = new Chart(myDoughnutChart, {
+let NewDoughnutChart = new Chart(doughnutChart, {
     type: 'doughnut',
     data: {
         labels: ["Phones", "Tablets", "Desktop"],
