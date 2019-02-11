fcc91b381b8b4264732a23e9650c56f291a50d92
TriantafyllosFamprikatzis <alkisfab@gmail.com>
2019-02-11 17:00:31 +0200

    side nav has scroll functionality

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 //VARIABLES
+
+//Chart Variables
 const lineChart = document.getElementById('lineChart').getContext('2d');
 const barChart = document.getElementById('barChart').getContext('2d');
 const doughnutChart = document.getElementById('doughnutChart').getContext('2d');
@@ -7,23 +9,69 @@ const hourChart = document.getElementById('hour');
 const dayChart = document.getElementById('day');
 const weekChart = document.getElementById('week');
 const monthChart = document.getElementById('month');
-
-const sendButton = document.getElementById('send');
-const deleteButton = document.getElementById('btn');
-const alertBar = document.getElementById('alert');
-
-const message = document.getElementById('textarea');
-const user = document.getElementById('search-bottom');
-
 const chartButtons = document.querySelectorAll(".li");
 
-// Alert Bar, hides when clicked 
+//For Alert on sent
+const sendButton = document.getElementById('js-send');
+const message = document.getElementById('js-textarea');
+const user = document.getElementById('js-search-bottom');
+
+
+
+
+//NAV SCROLL BUTTONS
+//Nav scroll when clicked for top page
+const topLink = document.querySelector("a[href='#top']")
+topLink.addEventListener("click", function(e) {
+    target = document.getElementById("top");
+    if (window.scrollTo) {
+        e.preventDefault();
+        window.scrollTo({"behavior": "smooth", "top": target.offsetTop});
+    }
+})
+//Nav scroll when clicked for traffic
+const trafficLink = document.querySelector("a[href='#traffic']")
+trafficLink.addEventListener("click", function(e) {
+    target = document.getElementById("traffic");
+    if (window.scrollTo) {
+        e.preventDefault();
+        window.scrollTo({"behavior": "smooth", "top": target.offsetTop});
+    }
+})
+//Nav scroll when clicked for activity
+const activityLink = document.querySelector("a[href='#activity']")
+activityLink.addEventListener("click", function(e) {
+    target = document.getElementById("activity");
+    if (window.scrollTo) {
+        e.preventDefault();
+        window.scrollTo({"behavior": "smooth", "top": target.offsetTop});
+    }
+})
+//Nav scroll when clicked for settings
+const settingsLink = document.querySelector("a[href='#settings']")
+settingsLink.addEventListener("click", function(e) {
+    target = document.getElementById("settings");
+    if (window.scrollTo) {
+        // e.preventDefault();
+        window.scrollTo({"behavior": "smooth", "top": target.offsetTop});
+    }
+})
+
+
+
+//ALERT BAR
+//Hides when clicked 
+const deleteButton = document.getElementById('js-btn');
 deleteButton.addEventListener('click', () => {
+ const alertBar = document.getElementById('js-alert');
  alertBar.style.display = 'none';
 });
 
+
+
+//NOTIFICATION BOX
 //Bell pop up notification
-const bell = document.getElementById('bell');
+const bell = document.getElementById('js-bell');
 bell.addEventListener('click', () => {
     if(popUpMenu.style.display === 'none') {
       popUpMenu.style.display = 'block';
@@ -31,18 +79,16 @@ bell.addEventListener('click', () => {
         popUpMenu.style.display = 'none';
     }
 });
-
 //When user deletes all notifications a message li appears
 function checkIfNotificationsEmpty() {
-    const alertBell = document.getElementById('alert-on-bell');
+    const alertBell = document.getElementById('alert-bell');
     const liList = document.getElementById('notif-menu').getElementsByTagName('li');
     if ( liList.length === 3) {
         setTimeout(() =>{
             alertBell.style.display = 'block';
-        }, 751);
+        }, 450);
     }
 }
-
 // User can delete notifications
 const popUpMenu = document.getElementById('notif-menu');    
 popUpMenu.addEventListener ('click', (e) => {
@@ -50,10 +96,9 @@ if (e.target.tagName === 'BUTTON') {
     e.target.parentNode.classList.add('removed-item');
     setTimeout( () =>{
         e.target.parentNode.remove(e);
-    }, 750); 
+    }, 450); 
  } checkIfNotificationsEmpty();
 });
-
 // Alert on Send button
 /*When user press to submit the form, it displays a confirmation that the message was sent. 
 Error messages are displayed if a user isn’t selected or message field is empty.*/
@@ -69,6 +114,9 @@ sendButton.addEventListener('click', () => {
  }
 });
 
+
+
+//CHARTS
 //Chart Buttons
 /*Loop through the buttons
 When the page loads add class to weekChart 
